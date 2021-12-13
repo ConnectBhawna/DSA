7a8c98fe11836454f0671f8d609a76571744aa0e
Bhawna chauhan <bhawnabc2002@gmail.com>
2021-12-13 22:06:44 +0530

    done with today's 2 problem

---
+++ Codeforces/Problem1328A.cpp
@@ -0,0 +1,21 @@
+/*
+https://codeforces.com/problemset/problem/1328/A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        if(a%b == 0){
+            cout<<0<<endl;
+        }
+        else{
+            int w=a%b;
+            cout<<b-w<<endl;
+        }
+    }
+    return 0;
+}
