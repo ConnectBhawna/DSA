bb0946b7cc08555e100503d95751cfa00083008a
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-01-05 22:40:49 +0530

    done with 4 problem

---
+++ Codeforces/problem118b.cpp
@@ -0,0 +1,32 @@
+/*
+https://codeforces.com/problemset/problem/118/B
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int row = -n; row <= n; ++row)
+    {
+        int top = n - abs(row);
+        for (int i = 0; i < abs(row); ++i)
+        {
+            cout << "  ";
+        }
+        for (int i = 0; i < top; ++i)
+        {
+            cout << i << " ";
+        }
+        for (int i = top; i > 0; --i)
+        {
+            cout << i << " ";
+        }
+        cout << 0 << endl;
+    }
+    return 0;
+}
