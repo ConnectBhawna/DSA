8ec5994af81181cbdff2ead85613e36fa6072255
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-01-06 23:00:25 +0530

    4 problems

---
+++ Codeforces/problem535b.cpp
@@ -0,0 +1,24 @@
+/*
+https://codeforces.com/problemset/problem/535/B
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long l,i,x,sum,point;
+    string s;
+    while(cin>>s){
+        l=s.size();sum=0,x=1;
+        for(i=l-1;i>=0;i--){
+            if(s[i]=='7')
+                point=pow(2,x);
+            else
+                point=pow(2,x)-pow(2,x-1);
+
+                x++;
+                sum+=point;
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
