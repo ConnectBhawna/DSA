98b5fda74e955d83e72153cec258035f43c2ab81
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-01-10 22:21:01 +0530

    4 problem

---
+++ Codeforces/problem651b.cpp
@@ -0,0 +1,33 @@
+/*
+https://codeforces.com/problemset/problem/651/A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    long long i,j,n,m,a,b,cnt;
+    while(cin>>a>>b)
+    {
+        cnt=0;
+        while(a>0 && b>0)
+        {
+            if(a==1 &&b ==1)
+                break;
+            if(a>b)
+            {
+                a-=2;
+                b+=1;
+            }
+            else
+            {
+                a+=1;
+                b-=2;
+            }
+            cnt++;
+        }
+        cout<<cnt<<endl;
+    }
+
+    return 0;
+}
