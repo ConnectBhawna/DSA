09b4245abb5bac90169e713163c42e5935755ae8
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-01-26 23:09:34 +0530

    Day 7

---
+++ Leetcode/Day8.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long a, pas = 0;
+int main() {
+    cin >> a;
+    for (int i = 1; i <= a; i++) {
+        pas += pow(2, i);
+    }
+    cout << pas;
+}
