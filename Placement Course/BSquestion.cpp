589982f66c3e86c01bf5da12ddf3ced12aa03628
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-04-16 17:47:18 +0530

    Done with Finding pivot element

---
+++ Placement Course/BSquestion.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int getPivot(int arr[],int n){
+       int s=0;
+       int e = n-1;
+       int mid = s + (e-s)/2;
+
+       while(s<e){
+           if(arr[mid] >= arr[0]){
+               s = mid-1;
+           }
+           else{
+               s = mid;
+           }
+           mid = s+ (e-s)/2;
+       }
+       return s;
+
+}
+int main(){
+    int arr[5] = {9,18,17,1,3};
+    cout<<"Pivot is"<<getPivot(arr,5)<<endl;
+}
