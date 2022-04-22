a2a960fbe40d8170bb24010403cbd087e8785b9b
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-04-22 18:04:32 +0530

    Doing list

---
+++ Placement Course/STL-Lecture19/list.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int main(){
+    list<int> l;
+    l.push_back(1);
+    l.push_back(2);
+    l.push_back(3);
+    l.push_back(4);
+    l.push_back(5);
+    l.push_back(6);
+    l.push_back(7);
+
+    for(int i:l){
+        cout<<i<<" ";
+    }
+    
+
+    return 0;
+    
+}
