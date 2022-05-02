82b8f827b4fe62811b70deec14111f99e7e79841
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-05-02 12:41:34 +0530

    done with priority queue

--- Placement Course/STL-Lecture19/priorityQueue.cpp
+++ Placement Course/STL-Lecture19/priorityQueue.cpp
@@ -1,23 +1,45 @@
 #include<iostream>
-#include<stack>
+#include<queue>
 using namespace std;
 int main()
 {
-    stack<string> s;
-    s.push("Bhawna");
-    s.push("Vidhi");
-    s.push("Chauhan");
+    //Max-Heap
+    priority_queue<int> max;
+    //Min Heap
+    priority_queue<int,vector<int>,greater<int> > mini;
+    max.push(1);
+    max.push(3);
+    max.push(2);
+    max.push(0);
 
-    cout<<"Top element : "<<s.top()<<endl;
+    cout<<"SIze -> "<<max.size()<<endl;
+    int n  = max.size();
+    for(int i = 0; i < n;i++){
+        cout<<max.top()<<" ";
+        max.pop();
+    }cout<<endl;
 
-    s.pop();
-    cout<<"Top element after popping of element : "<<s.top()<<endl;
+    cout<<"Now doing for min Heap";
+    mini.push(5);
+    mini.push(0);
+    mini.push(4);
+    mini.push(1);
+    mini.push(3);
 
-    cout<<"size of stack : "<<s.size()<<endl;
+    cout<<"SIze -> "<<mini.size()<<endl;
+    int n2  = mini.size();
+    for(int i = 0; i < n2;i++){
+        cout<<mini.top()<<" ";
+        mini.pop();
+    }cout<<endl;
 
-    cout<<"Empty stack or not : "<<s.empty()<<endl;
+
+    cout<<"Is the priority queue empty : "<<mini.empty()<<endl;
 
 
+
+
+    
 
     return 0;
 }
