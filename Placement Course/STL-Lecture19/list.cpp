5be7725d98bb1b8c1c63b6d8579ba6176a124194
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-05-02 09:53:29 +0530

    DOne with list in STL

--- Placement Course/STL-Lecture19/list.cpp
+++ Placement Course/STL-Lecture19/list.cpp
@@ -14,8 +14,26 @@ int main(){
     for(int i:l){
         cout<<i<<" ";
     }
-    
 
+    cout<<endl;
+    l.erase(l.begin());
+    cout<<"After Erase"<<endl;
+    for(int i:l){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    //cout<<"Delete from front"<<l.pop_front(1)<<endl;
+    //cout<<"Delete from back"<<l.pop_back(2)<<endl;
+
+    cout<<"Size of list is: "<<l.size()<<endl;
+
+    cout<<"Copy of above list into another one"<<endl;
+    list<int> n[l];
+    for(int i:n){
+        cout<<i<<" ";
+    }
+    
     return 0;
     //Today not well do from tomorrow onwards
 }
