eaa0a6695faf4f453032643952a1364f24b13194
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-05-03 10:04:41 +0530

    Done with map

--- Placement Course/STL-Lecture19/map.cpp
+++ Placement Course/STL-Lecture19/map.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<map>
 using namespace std;
+
+//Here all the operations take O(logn) time it's implementation 
+//done with trees that's why
 int main(){
 
     map<int,string> m;
@@ -17,5 +20,16 @@ int main(){
 
     cout<<"Finding 5 : "<<m.count(5)<<endl;
     cout<<"Finding -5 : "<<m.count(-5)<<endl;
+
+    m.erase(3);
+    cout<<"Map after erase : "<<endl;
+    for(auto i:m){
+        cout<< i.first <<" "<<i.second <<endl;
+    }cout<<endl;
+
+    auto it = m.find(5);
+    for(auto i = it;i<m.end();i++){
+      cout<<(*i).first<<endl;
+    }
   return 0;
 }
